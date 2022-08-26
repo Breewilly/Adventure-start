41206e12dd97b143f97717adbd22cfeb0917e160
Breewilly <112143805+Breewilly@users.noreply.github.com>
2022-08-25 22:39:07 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+Hello.java
+
+Brendan Williams
+8/25/2022
+CMSC 255
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Brendan Williams");
+		System.out.println("Hello.java");
+		System.out.println("Introduction to Java");
+		System.out.println("8/25/2022");
+		System.out.println("CMSC 255");
+		System.out.println();
+	}
+}
